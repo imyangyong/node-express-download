f315f88bbc2cf14c978f1b70a48cdb4652616384
杨勇 <1994101600@qq.com>
2019-10-29 15:21:23 +0800

    feat(favicon): 完成

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '全税研发部前端资源系统' });
+  res.render('index', { title: '前端项目资源' });
 });
 
 module.exports = router;
