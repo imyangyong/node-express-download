2d6cf215014700a4a7a44cc7d29035d22bb16053
杨勇 <1994101600@qq.com>
2019-12-17 22:05:34 +0800

    fix(delete): file readdirSync return Array 0 also true

--- routes/delete.js
+++ routes/delete.js
@@ -30,13 +30,13 @@ delRouter.get(/.*/, function(req, res, next) {
       res.end();
   } else if (params.length === 2 && fs.existsSync(path.resolve(process.cwd(), 'resources', params[1]))){
     var files = fs.readdirSync(path.resolve(process.cwd(), 'resources', params[1]));
-    if (files) {
+    if (files.length > 0) {
       res.writeHead(500, {'content-type': 'text/plain'});
       res.write('Project files exist, please delete all of them, then do this\n\n');
   
       res.end();
     } else {
-      del.sync(path.resolve(process.cwd(), 'resources', params[1]));
+      del.sync(path.resolve(process.cwd(), 'resources', params[1]), { force: true });
       res.writeHead(200, {'content-type': 'text/plain'});
       res.write('Delete success\n\n');
   
